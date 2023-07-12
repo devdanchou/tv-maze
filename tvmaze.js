76806c13ec8ec78380e0c1f443d7ac8124ec7a8e
Stuart Fleisher <stufleisher@gmail.com>
2023-07-12 15:39:52 -0400

    WIP displayShows

--- tvmaze.js
+++ tvmaze.js
@@ -12,35 +12,52 @@ const $searchForm = $("#searchForm");
  *    (if no image URL given by API, put in a default image URL)
  */
 
-
-
 async function getShowsByTerm(term) {
+  console.log('getShowByTerm called with',term);
   // ADD: Remove placeholder & make request to TVMaze search shows API.
   const showRequestBaseURL = "http://api.tvmaze.com/search/shows";
 
   // make a request and extract these data -> id, name, summary, image
-  const requestData = await axios.get(showRequestBaseURL, {params: {q:`[${term}]`}});
-  const {id, name, summary, image} = requestData.show.id;
+  const requestData = await axios.get(showRequestBaseURL, { params: { q: `[${term}]` } });
+  console.log('finished awaiting', requestData.data);
 
-  return [
-    {
-      id: exe,
-      name: "The Bletchley Circle",
-      summary:
-        `<p><b>The Bletchley Circle</b> follows the journey of four ordinary
-           women with extraordinary skills that helped to end World War II.</p>
-         <p>Set in 1952, Susan, Millie, Lucy and Jean have returned to their
-           normal lives, modestly setting aside the part they played in
-           producing crucial intelligence, which helped the Allies to victory
-           and shortened the war. When Susan discovers a hidden code behind an
-           unsolved murder she is met by skepticism from the police. She
-           quickly realises she can only begin to crack the murders and bring
-           the culprit to justice with her former friends.</p>`,
-      image:
-          "http://static.tvmaze.com/uploads/images/medium_portrait/147/369403.jpg"
-    }
-  ]
+  const allResults = [];
+
+  for (let i=0;i<requestData.data.length;i++){
+    allResults.push(
+      {
+      id:requestData.data[0].show.id,
+      name:requestData.data[0].show.name,
+      summary:requestData.data[0].show.summary,
+      image:requestData.data[0].show.image.medium,
+      }
+    );
+  }
+  console.log(allResults);
+  return [allResults];
 }
+
+
+/** +++++++++++++++++++++++++ OLD CODE FOR REFERENCE +++++++++++++++++*/
+// return [
+//   {
+//     id: exe,
+//     name: "The Bletchley Circle",
+//     summary:
+//       `<p><b>The Bletchley Circle</b> follows the journey of four ordinary
+//            women with extraordinary skills that helped to end World War II.</p>
+//          <p>Set in 1952, Susan, Millie, Lucy and Jean have returned to their
+//            normal lives, modestly setting aside the part they played in
+//            producing crucial intelligence, which helped the Allies to victory
+//            and shortened the war. When Susan discovers a hidden code behind an
+//            unsolved murder she is met by skepticism from the police. She
+//            quickly realises she can only begin to crack the murders and bring
+//            the culprit to justice with her former friends.</p>`,
+//     image:
+//       "http://static.tvmaze.com/uploads/images/medium_portrait/147/369403.jpg"
+//   }
+// ]
+// }
 
 
 /** Given list of shows, create markup for each and append to DOM.
@@ -49,14 +66,16 @@ async function getShowsByTerm(term) {
  * */
 
 function displayShows(shows) {
+  console.log('displayShows',shows);
   $showsList.empty();
 
   for (const show of shows) {
+    const imgURL = show.image ? show.image : "https://tinyurl.com/tv-missing";
     const $show = $(`
         <div data-show-id="${show.id}" class="Show col-md-12 col-lg-6 mb-4">
          <div class="media">
            <img
-              src="http://static.tvmaze.com/uploads/images/medium_portrait/160/401704.jpg"
+              src=${imgURL}
               alt="Bletchly Circle San Francisco"
               class="w-25 me-3">
            <div class="media-body">
@@ -87,7 +106,7 @@ async function searchShowsAndDisplay() {
   displayShows(shows);
 }
 
-$searchForm.on("submit", async function handleSearchForm (evt) {
+$searchForm.on("submit", async function handleSearchForm(evt) {
   evt.preventDefault();
   await searchShowsAndDisplay();
 });

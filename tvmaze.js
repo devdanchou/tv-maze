1d25adaba878d0dd3be8a009e36b52031a7b7163
Daniel Chou <daniel.cw.chou@gmail.com>
2023-07-12 15:00:09 -0400

    wip: getShowsByTerm

--- tvmaze.js
+++ tvmaze.js
@@ -12,21 +12,28 @@ const $searchForm = $("#searchForm");
  *    (if no image URL given by API, put in a default image URL)
  */
 
-async function getShowsByTerm( /* term */) {
+
+
+async function getShowsByTerm(term) {
   // ADD: Remove placeholder & make request to TVMaze search shows API.
+  const showRequestBaseURL = "http://api.tvmaze.com/search/shows";
+
+  // make a request and extract these data -> id, name, summary, image
+  const requestData = await axios.get(showRequestBaseURL, {params: {q:`[${term}]`}});
+  const {id, name, summary, image} = requestData.show.id;
 
   return [
     {
-      id: 1767,
+      id: exe,
       name: "The Bletchley Circle",
       summary:
-        `<p><b>The Bletchley Circle</b> follows the journey of four ordinary 
+        `<p><b>The Bletchley Circle</b> follows the journey of four ordinary
            women with extraordinary skills that helped to end World War II.</p>
-         <p>Set in 1952, Susan, Millie, Lucy and Jean have returned to their 
-           normal lives, modestly setting aside the part they played in 
-           producing crucial intelligence, which helped the Allies to victory 
+         <p>Set in 1952, Susan, Millie, Lucy and Jean have returned to their
+           normal lives, modestly setting aside the part they played in
+           producing crucial intelligence, which helped the Allies to victory
            and shortened the war. When Susan discovers a hidden code behind an
-           unsolved murder she is met by skepticism from the police. She 
+           unsolved murder she is met by skepticism from the police. She
            quickly realises she can only begin to crack the murders and bring
            the culprit to justice with her former friends.</p>`,
       image:
@@ -48,9 +55,9 @@ function displayShows(shows) {
     const $show = $(`
         <div data-show-id="${show.id}" class="Show col-md-12 col-lg-6 mb-4">
          <div class="media">
-           <img 
-              src="http://static.tvmaze.com/uploads/images/medium_portrait/160/401704.jpg" 
-              alt="Bletchly Circle San Francisco" 
+           <img
+              src="http://static.tvmaze.com/uploads/images/medium_portrait/160/401704.jpg"
+              alt="Bletchly Circle San Francisco"
               class="w-25 me-3">
            <div class="media-body">
              <h5 class="text-primary">${show.name}</h5>
